7d9b5750a65f0e2303e4a1492e3555a2017394ef
Ege Aydemir <egeaydemirr97@gmail.com>
2022-09-24 13:29:15 +0300

    Initial Commit

---
+++ RestartAppSwiftUI/RestartAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppSwiftUIApp.swift
+//  RestartAppSwiftUI
+//
+//  Created by Ege Aydemir on 24.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
