4a8778d748c2ca1631acfe35c4bf4b091b3a2089
Ege Aydemir <egeaydemirr97@gmail.com>
2022-09-25 20:59:16 +0300

    Deformable Shape

--- RestartAppSwiftUI/Screens/OnboardingView.swift
+++ RestartAppSwiftUI/Screens/OnboardingView.swift
@@ -66,7 +66,7 @@ struct OnboardingView: View {
                     HStack {
                         Capsule()
                             .fill(Color("ColorRed"))
-                            .frame(width: 80)
+                            .frame(width: buttonOffset + 80)
                         Spacer()
                     }
                     //4. Circle (Draggable)
